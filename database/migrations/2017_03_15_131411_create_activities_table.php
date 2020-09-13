df865b6f697340d6930140353bdc16b07a1d9675
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-09-13 19:25:30 -0300

    fix laravel8 compatibility

--- database/migrations/2017_03_15_131411_create_activities_table.php
+++ database/migrations/2017_03_15_131411_create_activities_table.php
@@ -1,40 +1,42 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class CreateActivitiesTable extends Migration {
+class CreateActivitiesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('activities', function(Blueprint $table) {
-            $table->increments('id');
-            $table->string('causer')->index();
-            $table->string('type')->index();
-            $table->string('indentifier')->index();
-            $table->text('data')->nullable();
-			
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('activities')) {
+            Schema::create('activities', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('causer')->index();
+                $table->string('type')->index();
+                $table->string('indentifier')->index();
+                $table->text('data')->nullable();
+            
 
-            // activitable
-            $table->string('activitable_id');
-            $table->string('activitable_type');
-			$table->timestamps();
-        });
-	}
+                // activitable
+                $table->string('activitable_id');
+                $table->string('activitable_type');
+                $table->timestamps();
+            });
+        }
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('activities');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
 }
