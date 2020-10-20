1b55288633e06cb4c9e1ff16c537205a3efb16b6
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2020-10-20 04:14:02 -0300

    working in windows

--- src/TransmissorProvider.php
+++ src/TransmissorProvider.php
@@ -91,7 +91,7 @@ class TransmissorProvider extends ServiceProvider
         /**
          * Transmissor; Routes
          */
-        $this->loadRoutesForRiCa(__DIR__.'/../routes');
+        $this->loadRoutesForRiCa(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'routes');
     }
 
     /**
@@ -176,7 +176,7 @@ class TransmissorProvider extends ServiceProvider
         $this->publishes(
             [
             // Paths
-            $this->getPublishesPath('config/sitec') => config_path('sitec'),
+            $this->getPublishesPath('config'.DIRECTORY_SEPARATOR.'sitec') => config_path('sitec'),
             ],
             ['config',  'sitec', 'sitec-config']
         );
@@ -197,7 +197,7 @@ class TransmissorProvider extends ServiceProvider
         $this->loadViewsFrom($viewsPath, 'transmissor');
         $this->publishes(
             [
-            $viewsPath => base_path('resources/views/vendor/transmissor'),
+            $viewsPath => base_path('resources'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'transmissor'),
             ],
             ['views',  'sitec', 'sitec-views']
         );
@@ -208,7 +208,7 @@ class TransmissorProvider extends ServiceProvider
         // Publish lanaguage files
         $this->publishes(
             [
-            $this->getResourcesPath('lang') => resource_path('lang/vendor/transmissor')
+            $this->getResourcesPath('lang') => resource_path('lang'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'transmissor')
             ],
             ['lang',  'sitec', 'sitec-lang', 'translations']
         );
@@ -227,7 +227,7 @@ class TransmissorProvider extends ServiceProvider
             'logging.channels.sitec-transmissor',
             [
             'driver' => 'single',
-            'path' => storage_path('logs/sitec-transmissor.log'),
+            'path' => storage_path('logs'.DIRECTORY_SEPARACTOR.'sitec-transmissor.log'),
             'level' => env('APP_LOG_LEVEL', 'debug'),
             ]
         );
