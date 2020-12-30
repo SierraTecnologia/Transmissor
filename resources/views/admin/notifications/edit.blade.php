38f0c4f3da1b7eee51e082e6df84f853a9b73e97
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2020-12-30 12:56:48 -0300

    fix bugs

--- resources/views/admin/notifications/edit.blade.php
+++ resources/views/admin/notifications/edit.blade.php
@@ -36,7 +36,7 @@
                 <div class="raw-margin-top-24 form-group">
                     <div class="btn-toolbar justify-content-between">
                         <button type="submit" class="btn btn-primary float-right">Update</button>
-                        <a href="/admin/notifications" class="btn btn-secondary">Cancel</a>
+                        <a href="/admin/notifications" class="btn btn-secondary">{{ __('pedreiro::generic.cancel') }}</a>
                     </div>
                 </div>
 
